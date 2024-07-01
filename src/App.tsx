2f567496898f7096ff62339810b285a7785983d0
Mauricio Erazo <antonierazo13@hotmail.com>
2024-07-01 18:25:09 +0200

    feat: made navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold ">Hello LinkedIn!</h1>
-      <button className="btn btn-accent btn-outline">Push me</button>
-    </div>
+    <Router>
+      <Navbar title="Github Finder" />
+    </Router>
   );
 }
 
