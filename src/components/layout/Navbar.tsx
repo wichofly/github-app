94e12bcd6027041af1bef906012d63f72b42a592
Mauricio Erazo <antonierazo13@hotmail.com>
2024-07-02 00:24:13 +0200

    feat: Routes with Route added

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -11,17 +11,17 @@ const Navbar = ({ title }: Prop) => {
       <div className="container mx-auto">
         <div className="flex-none px-2 mx-2">
           <FaGithub className="inline pr-2 text-3xl" />
-          <Link to="/" className="text-lg font-bold align-middle">
+          <Link to="/" className="text-xl font-bold align-middle">
             {title}
           </Link>
         </div>
 
-        <div className="flex-1 px-2 mx-2">
-          <div className="flex justify-end">
-            <Link to="/" className="btn btn-ghost btn-sm">
+        <div className="flex-1 px-2 mx-2 ">
+          <div className="flex justify-end ">
+            <Link to="/" className="btn btn-ghost btn-sm text-xl">
               Home
             </Link>
-            <Link to="/about" className="btn btn-ghost btn-sm">
+            <Link to="/about" className="btn btn-ghost btn-sm text-xl">
               About
             </Link>
           </div>
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-6xl">Welcome to LinkedIn!</h1>
+    </div>
+  );
+};
+
+export default Home;
