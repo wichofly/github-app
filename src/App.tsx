19804fe58f9088e53713bf164d6a913a054e88e0
Mauricio Erazo <antonierazo13@hotmail.com>
2024-07-01 17:21:03 +0200

    fix: corrected the code to use daisyui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css';
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold ">Hello LinkedIn!</h1>
+      <button className="btn btn-accent btn-outline">Push me</button>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import daisyui from 'daisyui'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -7,8 +9,9 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [
-    require('daisyui'),
-  ],
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["dark", "light", "cupcake", 'cyberpunk'],
+  },
 }
 
