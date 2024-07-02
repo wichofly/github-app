5126cf6c83b55f372ff445585fc948f2a0415e2f
Mauricio Erazo <antonierazo13@hotmail.com>
2024-07-02 23:09:02 +0200

    style: placed the span to the center

--- src/components/users/UserResults.tsx
+++ src/components/users/UserResults.tsx
@@ -38,7 +38,11 @@ const UserResults = () => {
       </div>
     );
   } else {
-    return <span className="loading loading-bars loading-lg"></span>;
+    return (
+      <div className="flex items-center justify-center">
+        <span className="loading loading-bars loading-lg"></span>;
+      </div>
+    );
   }
 };
 
