02152ba25c3bd8743e3eb70003b09d33a955092e
Mauricio Erazo <antonierazo13@hotmail.com>
2024-07-04 15:10:52 +0200

    feat: made onSubmit and onChange arrays

--- src/components/users/UserSearch.tsx
+++ src/components/users/UserSearch.tsx
@@ -1,14 +1,32 @@
+import { useState } from 'react';
+
 const UserSearch = () => {
+  const [text, setText] = useState('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    
+    if (text === '') {
+      alert('Please enter something to search.');
+    } else {
+      // Proceed with the search logic
+      console.log('Searching for:', text);
+      setText('');
+    }
+  };
+
   return (
     <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
       <div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-control">
             <div className="relative">
               <input
                 type="text"
                 className="w-full pr-40 bg-gray-200 input input-lg text-black"
                 placeholder="Search"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
               />
               <button
                 type="submit"
