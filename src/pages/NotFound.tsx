f2fb5d8ab682e56545556fe42f0d64b84ab48217
Mauricio Erazo <antonierazo13@hotmail.com>
2024-07-14 17:01:17 +0200

    style: change text color

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 const NotFound = () => {
   return (
     <div className="hero">
-      <div className="text-center">
+      <div className="text-center text-slate-50">
         <div className="max-w-lg">
           <h1 className="text-8xl font-bold mb-8">Oops!</h1>
         </div>
         <p className="text-5xl mb-8">404 - Page not found</p>
-        <Link to="/" className="btn btn-primary btn-lg">
+        <Link to="/" className="btn btn-primary btn-lg text-slate-50">
           <FaHome className="mr-2 text-3xl" />
           Back to Home
         </Link>
