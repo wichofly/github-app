8fb1a327f94338a8b5ab2c0d9fbdb12166b687b7
Mauricio Erazo <antonierazo13@hotmail.com>
2024-07-19 00:31:34 +0200

    refactor: axios is used to clean up

--- src/action/GithubActions.ts
+++ src/action/GithubActions.ts
@@ -1,34 +1,30 @@
+import axios from 'axios';
 import { getGithubSearchUrl, getGithubToken } from '../service/config';
 
-const githubSearchUrl = getGithubSearchUrl();
+const githubURL = getGithubSearchUrl();
 const githubToken = getGithubToken();
 
+const github = axios.create({
+  baseURL: githubURL,
+  headers: { Authorization: `token ${githubToken}` },
+});
+
 export const searchUsers = async (text: string) => {
-  const response = await fetch(`${githubSearchUrl}/search/users?q=${text}`, {
-    headers: {
-      Authorization: `token ${githubToken}`,
-    },
-  });
-
-  const data = await response.json();
-
-  return data.items;
+  const response = await github(`/search/users?q=${text}`);
+  return response.data.items;
 };
 
 // Get single User
 export const getUser = async (login: string) => {
-  const response = await fetch(`${githubSearchUrl}/users/${login}`, {
-    headers: {
-      Authorization: `token ${githubToken}`,
-    },
-  });
-
-  if (response.status === 404) {
-    document.location.href = '/notfound';
-  } else {
-    const data = await response.json();
-
-    return data;
+  try {
+    const response = await github(`/users/${login}`);
+    return response.data;
+  } catch (error: any) {
+    if (error.response && error.response.status === 404) {
+      document.location.href = '/notfound';
+    } else {
+      throw error;
+    }
   }
 };
 
@@ -39,14 +35,6 @@ export const getUserRepos = async (login: string) => {
     per_page: '10',
   });
 
-  const response = await fetch(
-    `${githubSearchUrl}/users/${login}/repos?${params}`,
-    {
-      headers: {
-        Authorization: `token ${githubToken}`,
-      },
-    }
-  );
-  const data = await response.json();
-  return data;
+  const response = await github(`/users/${login}/repos?${params}`);
+  return response.data;
 };
