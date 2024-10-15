165829ea10ffc85115d3e58bce1f59b63a5bc5ff
Mauricio Erazo <antonierazo13@hotmail.com>
2024-10-15 17:24:20 +0200

    refactor: added icons and links to find me.

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,14 +1,53 @@
-import { FaGithubAlt } from 'react-icons/fa';
+import { FaGithub, FaGithubAlt, FaLaptop, FaLinkedin } from 'react-icons/fa';
 
 const Footer = () => {
   const footerYears = new Date().getFullYear();
 
   return (
-    <footer className="footer p-5 bg-neutral-700 footer-center">
-      <FaGithubAlt className="text-5xl text-white" />
-      <p className="text-white text-xl">
-        Copyrights &copy; {footerYears} All Rights Reserved by Techinfo
-      </p>
+    <footer className="bg-neutral-700 py-3">
+      <div className="container mx-auto p-2 text-white flex justify-between items-center">
+        <div className="flex items-center space-x-3">
+          <FaGithubAlt className="text-5xl " />
+          <p className=" text-2xl font-light">
+            Copyrights &copy; {footerYears} All Rights Reserved by Techinfo
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <p className=" text-2xl font-semibold mb-4">Find me on</p>
+          <div className="flex space-x-8">
+            <a
+              href="https://www.linkedin.com/in/mauricio-erazo-/"
+              target="_blank"
+              rel="noopener"
+              className="hover:text-blue-400 transition duration-300"
+              aria-label="LinkedIn"
+            >
+              <FaLinkedin className="w-8 h-8" />
+            </a>
+
+            <a
+              href="https://github.com/wichofly/tipCalculator-app"
+              target="_blank"
+              rel="noopener"
+              className="hover:text-gray-500 transition duration-300"
+              aria-label="GitHub"
+            >
+              <FaGithub className="w-8 h-8" />
+            </a>
+
+            <a
+              href="https://wichofly.github.io/portfolio-website/"
+              target="_blank"
+              rel="noopener"
+              className="hover:text-slate-400 transition duration-300"
+              aria-label="Portfolio"
+            >
+              <FaLaptop className="w-8 h-8" />
+            </a>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -8,15 +8,15 @@ interface Prop {
 const Navbar = ({ title }: Prop) => {
   return (
     <nav className="navbar mb-12 shadow-lg bg-neutral-800 text-white">
-      <div className="container mx-auto">
-        <div className="flex-none px-2 mx-2">
+      <div className="container mx-auto p-2">
+        <div className="flex-none">
           <FaGithub className="inline pr-2 text-3xl" />
           <Link to="/" className="text-xl font-bold align-middle">
             {title}
           </Link>
         </div>
 
-        <div className="flex-1 px-2 mx-2 ">
+        <div className="flex-1">
           <div className="flex justify-end ">
             <Link to="/" className="btn btn-ghost btn-sm text-xl">
               Home
