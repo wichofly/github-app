a2e0cdc70a5622d10f441e709c4b087305e4a88e
Mauricio Erazo <antonierazo13@hotmail.com>
2025-01-18 20:08:48 +0100

    fix: extend User interface with fields to clean up User component

--- src/interfaces/interface.ts
+++ src/interfaces/interface.ts
@@ -2,6 +2,17 @@ export interface User {
   id: number;
   login: string;
   avatar_url: string;
+  html_url: string; // URL to the user's GitHub profile
+  name?: string; 
+  type: string; // Account type (e.g., "User" or "Organization")
+  location?: string; 
+  bio?: string;
+  blog?: string; 
+  followers: number; 
+  following: number; 
+  public_repos: number; 
+  public_gists: number; 
+  hireable?: boolean; // Whether the user is hireable (optional)
 }
 
 export interface Repo {
--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -22,7 +22,7 @@ const User = () => {
         dispatch({ type: ACTION_TYPES.SET_LOADING, payload: false });
       }
     };
-
+    
     getUserData();
   }, [params.login, dispatch]);
 
