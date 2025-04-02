c3f5c2ecd60ccb316ff98ea533b68cd429b00b16
Mauricio Erazo <antonierazo13@hotmail.com>
2025-04-02 18:08:49 +0200

    fix: improve Footer component layout for better alignment and responsiveness

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="bg-neutral-700 py-6">
       <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center space-y-6 md:space-y-0">
-        <div className="flex flex-col items-center text-center md:text-left">
+        <div className="flex flex-col items-center text-center lg:flex-row lg:items-center lg:space-x-4">
           <FaGithubAlt className="text-4xl sm:text-5xl md:text-5xl text-white" />
           <p className="text-lg sm:text-xl md:text-2xl font-light text-white">
             Copyrights &copy; {footerYears} All Rights Reserved by Techinfo
