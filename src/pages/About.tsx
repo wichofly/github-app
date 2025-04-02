1a501fd5bff399881b2468e6ec701758e45303a4
Mauricio Erazo <antonierazo13@hotmail.com>
2025-04-02 16:34:03 +0200

    fix: update About page description and clean up User component code

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,7 +3,7 @@ const About = () => {
     <div>
       <h1 className="text-6xl mb-4">Github Finder</h1>
       <p className="mb-4 text-2xl font-light">
-        A React app to search GitHub profiles and see profile details.
+        A React app to search GitHub profiles and see profile details. Have fun! 😉
       </p>
     </div>
   );
